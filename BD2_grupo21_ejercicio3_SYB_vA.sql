dd5060d892f34ebf669df146bc5c6fc3ca08ee54
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-10-07 21:23:20 -0500

    added proveedor table

--- BD2_grupo21_ejercicio3_SYB_vA.sql
+++ BD2_grupo21_ejercicio3_SYB_vA.sql
@@ -4,9 +4,24 @@
 /* Crear una tabla que contenga las características presentadas en el
 enunciado del problema e incluya las columnas digitador y fecha */
 
+CREATE TABLE Proveedor
+(
+    productoID INTEGER,
+    empleadoID INTEGER,
+    proveeedorID INTEGER,
+    nombre VARCHAR(30),
+    nit BIGINT,
+    numeroTelefonico BIGINT,
+    numeroCentroLlamadas BIGINT,
+    direccion VARCHAR(30),
+    nombreGerente VARCHAR(30),
+    documentoGerente BIGINT,
+    telefonoGerente BIGINT,
+)
+
 CREATE TABLE Empleado
 (
-    identificacion BIGINT,
+    empleadoID INTEGER,
     nombres VARCHAR(30),
     apellidos VARCHAR(30),
     telefonoFijoEmpleado BIGINT,
@@ -30,37 +45,41 @@ CREATE TABLE Empleado
 la tabla recién creada y que excluya las columnas digitador y fecha */
 
 
-CREATE VIEW VISTA_1 AS
-SELECT
-    identificacion,
-    nombres,
-    apellidos,
-    telefonoFijoEmpleado,
-    telefonoMovilEmpleado,
-    dirreccion,
-    genero,
-    correo,
-    fechaIngreso,
-    departamentoEmpresa,
-    cargoEmpresa,
-    salarioMensual,
-    comision,
-    identificacionCliente,
-    nombreCliente,
-    telefonoCliente
+CREATE VIEW VISTA_1
+AS
+    SELECT
+        identificacion,
+        nombres,
+        apellidos,
+        telefonoFijoEmpleado,
+        telefonoMovilEmpleado,
+        dirreccion,
+        genero,
+        correo,
+        fechaIngreso,
+        departamentoEmpresa,
+        cargoEmpresa,
+        salarioMensual,
+        comision,
+        identificacionCliente,
+        nombreCliente,
+        telefonoCliente
     FROM Empleado
 
 /* Crear la vista VISTA_2, que muestre todas las columnas de la tabla
 recién creada y que incluya un filtro por usuario */
 
-CREATE VIEW VISTA_2 AS
-SELECT *
-FROM Empleado
-WHERE digitador = user_name()
+CREATE VIEW VISTA_2
+AS
+    SELECT *
+    FROM Empleado
+    WHERE digitador = user_name()
 
 /* Añadir datos a su tabla NOMBRE_DE_LA_TABLA usando la VISTA_1 */
 
-INSERT INTO VISTA_1 VALUES (valor_1, valor_2, . . . , valor_n)
+INSERT INTO VISTA_1
+VALUES
+    (valor_1, valor_2, . . . , valor_n)
 
 /* Permitir que sus compañeros de grupo únicamente puedan a añadir datos
 a su tabla NOMBRE_DE_LA_TABLA utilizando la vista VISTA_1 */
@@ -80,30 +99,38 @@ GRANT INSERT ON VISTA_1 TO dabonilla
 /* Los compañeros de su grupos tienen que añadir datos a su tabla
 NOMBRE_DE_LA_TABLA, usando la vista cuenta.VISTA_1 */
 INSERT INTO cuenta_del_compañero.VISTA_1
-VALUES (valor_1, valor_2, . . . , valor_n)
+VALUES
+    (valor_1, valor_2, . . . , valor_n)
 /* Ver los datos que cada uno ingresó a la tabla NOMBRE_DE_LA_TABLA
 utilizando la vista cuenta.VISTA_2 */
-SELECT * FROM cuenta_del_compañero.VISTA_2
+SELECT *
+FROM cuenta_del_compañero.VISTA_2
 
 /* DATOS */
 /* Recursos Humanos, Contabilidad,
 Mercadeo, Sistemas e Informática, Ventas, Jurídico, Producción, Empaque. */
 
 /* SEBASTIAN */
-INSERT INTO VISTA_1 VALUES (valor_1, valor_2, . . . , valor_n)
+INSERT INTO VISTA_1
+VALUES
+    (valor_1, valor_2, . . . , valor_n)
 
 /* DIEGO */
 INSERT INTO cuenta_del_compañero.VISTA_1
-VALUES (valor_1, valor_2, . . . , valor_n)
+VALUES
+    (valor_1, valor_2, . . . , valor_n)
 
 /* ALEJANDRA */
 INSERT INTO cuenta_del_compañero.VISTA_1
-VALUES (valor_1, valor_2, . . . , valor_n)
+VALUES
+    (valor_1, valor_2, . . . , valor_n)
 
 /* DANIEL */
 INSERT INTO cuenta_del_compañero.VISTA_1
-VALUES (valor_1, valor_2, . . . , valor_n)
+VALUES
+    (valor_1, valor_2, . . . , valor_n)
 
 /* DAVID */
 INSERT INTO cuenta_del_compañero.VISTA_1
-VALUES (valor_1, valor_2, . . . , valor_n)
+VALUES
+    (valor_1, valor_2, . . . , valor_n)
