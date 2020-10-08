4f5accb0f71bf679b9ee6decf4c37b77d04d94d4
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-10-08 06:54:25 -0500

    Co-authored-by: davidsilvasum@gmail.com <davidsilvasum@gmail.com>

--- BD2_grupo21_ejercicio3_ORA_vA.sql
+++ BD2_grupo21_ejercicio3_ORA_vA.sql
@@ -11,9 +11,152 @@ create table Cliente
     listaEspera nchar(2),
     representanteLegal varchar (50) not null,
     Cedula int not null,
+    digitador VARCHAR(15) default USER NOT NULL,
+    fecha DATE default sysdate NOT NULL
     constraint PK_ncliente primary key(clienteID)
 );
 create sequence numeroCliente minvalue 1 start with 1;
+/* Crear la vista VISTA_Cliente_Edit que muestre las columnas que usted definió en
+la tabla recién creada y que excluya las columnas digitador y fecha */
+
+CREATE view VISTA_Cliente_Edit AS SELECT clienteID,
+                                    NIT,
+                                    Nombre,
+                                    Dirección,
+                                    Ciudad,
+                                    Teléfono,
+                                    Producto,
+                                    listaEspera,
+                                    representanteLegal,
+                                    Cedula
+                                    FROM Cliente;
+
+/* Crear la VISTA_Cliente_View, que muestre todas las columnas de la tabla
+recién creada y que incluya un filtro por usuario */
+
+CREATE OR replace view VISTA_Cliente_View 
+(
+    clienteID,
+    NIT,
+    Nombre,
+    D,
+    telefonoMovilEmpleado,
+    dirreccion,
+    genero,
+    correo,
+    fechaIngreso,
+    departamentoEmpresa,
+    cargoEmpresa,
+    salarioMensual,
+    comision,
+    identificacionCliente,
+    nombreCliente,
+    telefonoCliente,
+    digitador, 
+    fecha
+)
+AS SELECT 
+    identificacion,
+    nombres,
+    apellidos,
+    telefonoFijoEmpleado,
+    telefonoMovilEmpleado,
+    dirreccion,
+    genero,
+    correo,
+    fechaIngreso,
+    departamentoEmpresa,
+    cargoEmpresa,
+    salarioMensual,
+    comision,
+    identificacionCliente,
+    nombreCliente,
+    telefonoCliente,
+    digitador,
+    to_char(fecha, 'dd-mm-yyyy" "hh24:mi:ss')
+FROM Empleado
+WHERE digitador = USER;
+
+/* Permitir acceso a los compañeros de grupo */
+
+GRANT SELECT ON VISTA_Cliente_View TO negarzonc;
+GRANT INSERT ON VISTA_Cliente_Edit TO negarzonc;
+GRANT SELECT ON VISTA_Cliente_Edit TO negarzonc;
+
+GRANT SELECT ON VISTA_Cliente_View TO dabonilla;
+GRANT INSERT ON VISTA_Cliente_Edit TO dabonilla;
+GRANT SELECT ON VISTA_Cliente_Edit TO dabonilla;
+
+GRANT SELECT ON VISTA_Cliente_View TO maarojasga;
+GRANT INSERT ON VISTA_Cliente_Edit TO maarojasga;
+GRANT SELECT ON VISTA_Cliente_Edit TO maarojasga;
+
+
+/*INSERTAR DATOS*/
+
+insert into cuenta_del_compañero.VISTA_Cliente_Edit
+values (
+numeroCliente.nextval,
+122541,
+'Taller doña Gloria S.A.S',
+'Calle 111 #34-02',
+'Bogotá',
+'tallerdGloria@gmail.com',
+3111425,
+'Llantas',
+'Si',
+'Gloria Cupa',
+41125147,
+);
+
+insert into cuenta_del_compañero.VISTA_Cliente_Edit
+values (
+numeroCliente.nextval,
+425154,
+'Jabones para bebe S.A.S',
+'Calle 1 #02-02',
+'Bogotá',
+'jbebes@gmail.com',
+4555247,
+'Glicerina',
+'Si',
+'Diego Hernandez',
+47454856,
+);
+
+insert into cuenta_del_compañero.VISTA_Cliente_Edit
+values (
+numeroCliente.nextval,
+14582,
+'Tornillos S.A.S',
+'Carrera 10 #14-22',
+'Bogotá',
+'torni@gmail.com',
+4125864,
+'Chazos',
+'No',
+'Daniela Leal',
+41253658,
+);
+
+/*Inseretar con un error*/
+
+insert into cuenta_del_compañero.VISTA_Cliente_Edit
+values (
+1,     /*No se puede repetir numero en la llave primaria*/
+14582,
+'Tamales S.A.S',
+'Diagonal 78 #14-22',
+'Bogotá',
+'Tamal@gmail.com',
+123965,
+'Arroz',
+'No',
+'Victor Sierra',
+45823654,
+);
+
+
 
 
 
--- BD2_grupo21_ejercicio3_SYB_vA.sql
+++ BD2_grupo21_ejercicio3_SYB_vA.sql
@@ -8,24 +8,27 @@ enunciado del problema e incluya las columnas digitador y fecha */
 
 CREATE TABLE Empleado
 (
-    empleadoID INT,
-    nombres VARCHAR(30),
-    apellidos VARCHAR(30),
-    telefonoFijoEmpleado BIGINT,
+    empleadoID NUMERIC IDENTITY,
+    cédula BIGINT constraint  I_aaa unique clustered,
+    nombresApellidos VARCHAR(50),
     telefonoMovilEmpleado BIGINT,
-    dirreccion VARCHAR(50),
+    ciudad VARCHAR(15),
+    direccion VARCHAR(50),
     genero VARCHAR(10),
     correo VARCHAR(30),
     fechaIngreso DATETIME,
+    sucursal INT,
     departamentoEmpresa VARCHAR(25),
     cargoEmpresa VARCHAR(25),
-    salarioMensual MONEY,
-    comision MONEY,
-    identificacionCliente BIGINT NULL,
+    idJefeInmediato BIGINT,
+    sueldoFijo MONEY,
+    comision MONEY NULL,
+    clienteID BIGINT NULL,
     nombreCliente VARCHAR(30) NULL,
     telefonoCliente BIGINT NULL,
     digitador VARCHAR(15) default user_name() NOT NULL,
-    fecha DATETIME default getdate () NOT NULL
+    fecha DATETIME default getdate () NOT NULL,
+    constraint  PK_JJ primary key   (empleadoID)
 )
 
 /* Crear la vista VISTA_Empleado_Edit, que muestre las columnas que usted definió en
@@ -35,22 +38,90 @@ la tabla recién creada y que excluya las columnas digitador y fecha */
 CREATE VIEW VISTA_Empleado_Edit
 AS
     SELECT
-        empleadoID,
-        nombres,
-        apellidos,
-        telefonoFijoEmpleado,
-        telefonoMovilEmpleado,
-        dirreccion,
-        genero,
-        correo,
-        fechaIngreso,
-        departamentoEmpresa,
-        cargoEmpresa,
-        salarioMensual,
-        comision,
-        identificacionCliente,
-        nombreCliente,
-        telefonoCliente,
-        digitador,
-        fecha
+    cédula,
+    nombresApellidos,
+    telefonoMovilEmpleado,
+    ciudad,
+    direccion,
+    genero,
+    correo,
+    fechaIngreso,
+    sucursal,
+    departamentoEmpresa,
+    cargoEmpresa,
+    idJefeInmediato,
+    sueldoFijo,
+    comision,
+    clienteID,
+    nombreCliente,
+    telefonoCliente
+    FROM Empleado
+
+/* Crear la vista VISTA_Empleado_View, que muestre todas las columnas de la tabla
+recién creada y que incluya un filtro por usuario */
+
+CREATE VIEW VISTA_Empleado_View
+AS
+    SELECT *
+    FROM Empleado
+    WHERE digitador = user_name()
+
+/* Añadir datos a su tabla Empleado usando VISTA_Empleado_Edit */
+
+INSERT INTO VISTA_Empleado_Edit VALUES (valor_1, valor_2, . . . , valor_n)
+
+/* Permitir que sus compañeros de grupo únicamente puedan a añadir datos
+a su tabla Empleado utilizando la vista VISTA_Empleado_Edit */
+/* Permitir a sus compañeros de grupo, ver los datos que ellos añadieron
+a su tabla Empleado utilizando la vista VISTA_Empleado_View */
+
+GRANT SELECT ON VISTA_Empleado_View TO negarzonc
+GRANT INSERT ON VISTA_Empleado_Edit TO negarzonc
+GRANT SELECT ON VISTA_Empleado_Edit TO negarzonc
+
+GRANT SELECT ON VISTA_Empleado_View TO dsilvamo
+GRANT INSERT ON VISTA_Empleado_Edit TO dsilvamo
+GRANT SELECT ON VISTA_Empleado_Edit TO dsilvamo
+
+GRANT SELECT ON VISTA_Empleado_View TO dabonilla
+GRANT INSERT ON VISTA_Empleado_Edit TO dabonilla
+GRANT SELECT ON VISTA_Empleado_Edit TO dabonilla
+
+
+/* Los compañeros de su grupos tienen que añadir datos a su tabla
+Empleado, usando la vista cuenta.VISTA_Empleado_Edit*/
+INSERT INTO cuenta_del_compañero.VISTA_Empleado_Edit
+VALUES (valor_1, valor_2, . . . , valor_n)
+
+/* Ver los datos que cada uno ingresó a la tabla Empleado
+utilizando la vista cuenta.VISTA_Empleado_View */
+SELECT * FROM cuenta_del_compañero.VISTA_Empleado_View
+
+/* Ver los datos que todos ingresaron a la tabla Empleado
+utilizando la vista cuenta.VISTA_Empleado_Edit */
+SELECT * FROM cuenta_del_compañero.VISTA_Empleado_Edit
+
+
+/* DATOS */
+/* Recursos Humanos, Contabilidad,
+Mercadeo, Sistemas e Informática, Ventas, Jurídico, Producción, Empaque. */
+
+/* ALEJANDRA */
+INSERT INTO VISTA_Empleado_Edit VALUES (valor_1, valor_2, . . . , valor_n)
+
+/* DIEGO */
+INSERT INTO maarojasga.VISTA_Empleado_Edit
+VALUES (valor_1, valor_2, . . . , valor_n)
+
+/* SEBASTIAN */
+INSERT INTO cuenta_del_compañero.VISTA_Empleado_Edit
+VALUES (valor_1, valor_2, . . . , valor_n)
+
+/* DANIEL */
+INSERT INTO cuenta_del_compañero.VISTA_Empleado_Edit
+VALUES (valor_1, valor_2, . . . , valor_n)
+
+/* DAVID */
+INSERT INTO cuenta_del_compañero.VISTA_Empleado_Edit
+VALUES (valor_1, valor_2, . . . , valor_n)
 
