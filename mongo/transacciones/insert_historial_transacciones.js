12d8e97c9554b96fa9b705b0731dff5a943e44eb
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-12-01 10:54:39 -0500

    refactor transacciones

--- mongo/transacciones/insert_historial_transacciones.js
+++ mongo/transacciones/insert_historial_transacciones.js
@@ -6,18 +6,16 @@
 
 db.system.js.save({
     _id: "historial_transacciones",
-    value: function (precio, id_proveedor, id_producto, cantidad) {
-        if (db.proveedores.find({ "_id": ObjectId(id_proveedor) }).toArray().length <= 0) {
-            return "El proveedor no existe, crea uno primero o verifica el ID"
-        }
+    value: function (precio, id_producto, cantidad) {
         if (db.productos.find({ "_id": ObjectId(id_producto) }).toArray().length <= 0) {
             return "El producto no existe, crea uno primero o verifica el ID"
         }
+        let proveedor = db.productos.findOne({ "_id": ObjectId(id_producto) })
         db.registro_provedores.insertOne({
             "fecha": Date(),
             "precio": precio,
             "id_producto": id_producto,
-            "id_proveedor": id_proveedor,
+            "id_proveedor": proveedor['id_proveedor'],
             "cantidad": cantidad
         })
         return "Operacion exitosa"
